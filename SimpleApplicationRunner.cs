91ee1ced83a0f1f235ec8f07f1feb682fce2651d
riteelama <riteelama@gmail.com>
2025-11-10 20:54:07 +0545

    Add - Initial Commit

---
+++ SimpleApplicationRunner.cs
@@ -0,0 +1,31 @@
+﻿
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using WebDriverManager;
+using WebDriverManager.DriverConfigs.Impl;
+
+namespace SimpleApplication
+{
+    public class SimpleApplicationRunner
+    {
+        public static void Main(string[] args)
+        {
+            new DriverManager().SetUpDriver(new FirefoxConfig());
+
+            IWebDriver driver = new FirefoxDriver();
+            driver.Manage().Window.Maximize();
+
+            driver.Navigate().GoToUrl("https://github.com/");
+
+            IWebElement searchBox = driver.FindElement(By.CssSelector(".search-input") );
+            searchBox.Click();
+
+            IWebElement searchInput = driver.FindElement(By.CssSelector("#query-builder-test"));
+            searchInput.SendKeys("selenium");
+            searchInput.SendKeys(Keys.Enter);
+
+            driver.Quit();
+        }
+    }
+}
