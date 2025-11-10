ac2ce97e83b683f5e379f18db6e54fdbd3b0aee1
riteelama <riteelama@gmail.com>
2025-11-10 22:19:18 +0545

    Add - Assertion in NUnit test

---
+++ SimpleApplicationHome/SimpleApplicationHome/SimpleHomeApplicationRunner.cs
@@ -0,0 +1,27 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using WebDriverManager;
+using WebDriverManager.DriverConfigs.Impl;
+
+namespace SimpleApplication
+{
+    public class SimpleApplicationHomeClassRunner
+    {
+        public static void Main(string[] args)
+        {
+            new DriverManager().SetUpDriver(new FirefoxConfig());
+
+            IWebDriver driver = new FirefoxDriver();
+            driver.Manage().Window.Maximize();
+
+            driver.Navigate().GoToUrl("https://daraz.com.np");
+
+            IWebElement searchInput = driver.FindElement(By.CssSelector("#q"));
+
+            searchInput.SendKeys("laptop");
+            searchInput.SendKeys(Keys.Enter);
+
+            driver.Quit();
+        }
+    }
+}
